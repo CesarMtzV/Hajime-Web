57c89dd86afaef6d08acff04d73c422185c8d30f
CesarMtzV <mc.cesar.art@gmail.com>
2022-02-23 22:44:22 -0600

    First commit

---
+++ server/api/api.py
@@ -0,0 +1,12 @@
+from crypt import methods
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello world"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
