8cd5c40b2d510d357e1c063d7d32790d4b208323
CesarMtzV <mc.cesar.art@gmail.com>
2022-05-30 17:41:22 -0500

    Refactored About View page

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,11 +1,40 @@
 import React from "react";
 import NavBar from "../components/NavBar/NavBar";
 import { welcomeView_routes } from "../static/navbarRoutes";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import "../style.css";
+import { MemberCard } from "../components/MemberCard";
 
 export const AboutView = () => {
+    const members = [
+        {
+            name: "César Martínez",
+            role: "Programmer",
+            github_link: "https://github.com/CesarMtzV",
+            profile_picture:
+                "https://avatars.githubusercontent.com/u/42587773?v=4",
+        },
+        {
+            name: "Alex Vargas",
+            role: "Programmer",
+            github_link: "https://github.com/alexvargas01",
+            profile_picture:
+                "https://avatars.githubusercontent.com/u/43385032?v=4",
+        },
+        {
+            name: "Daniel David",
+            role: "Programmer",
+            github_link: "https://github.com/Danyboyyy",
+            profile_picture:
+                "https://avatars.githubusercontent.com/u/54334789?v=4",
+        },
+        {
+            name: "Santiago Cano",
+            role: "Programmer",
+            github_link: "https://github.com/canosantiago12",
+            profile_picture:
+                "https://avatars.githubusercontent.com/u/78878007?v=4",
+        },
+    ];
+
     return (
         <>
             <NavBar navbarRoutes={welcomeView_routes} />
@@ -19,106 +48,17 @@ export const AboutView = () => {
 
             <div className="container">
                 <div className="row text-center">
-                    {/* <!-- Team item --> */}
-                    <div className="col-xl-3 col-sm-6 mb-5">
-                        <div className="bg-white rounded shadow-sm py-5 px-4">
-                            <img
-                                src="https://avatars.githubusercontent.com/u/42587773?v=4"
-                                alt="César"
-                                width="100"
-                                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                    {members.map((item, key) => {
+                        return (
+                            <MemberCard
+                                key={key}
+                                name={item.name}
+                                role={item.role}
+                                github_link={item.github_link}
+                                profile_picture={item.profile_picture}
                             />
-                            <h5 className="mb-0">César Martínez</h5>
-                            <span className="small text-uppercase text-muted">
-                                Programmer
-                            </span>
-                            <ul className="social mb-0 list-inline mt-3">
-                                <li className="list-inline-item">
-                                    <a
-                                        href="https://github.com/CesarMtzV"
-                                        className="social-link"
-                                    >
-                                        <FontAwesomeIcon icon={faGithub} />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    {/* <!-- Team item --> */}
-                    <div className="col-xl-3 col-sm-6 mb-5">
-                        <div className="bg-white rounded shadow-sm py-5 px-4">
-                            <img
-                                src="https://avatars.githubusercontent.com/u/43385032?v=4"
-                                alt="César"
-                                width="100"
-                                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
-                            />
-                            <h5 className="mb-0">Alex Vargas</h5>
-                            <span className="small text-uppercase text-muted">
-                                Programmer
-                            </span>
-                            <ul className="social mb-0 list-inline mt-3">
-                                <li className="list-inline-item">
-                                    <a
-                                        href="https://github.com/alexvargas01"
-                                        className="social-link"
-                                    >
-                                        <FontAwesomeIcon icon={faGithub} />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    {/* <!-- Team item --> */}
-                    <div className="col-xl-3 col-sm-6 mb-5">
-                        <div className="bg-white rounded shadow-sm py-5 px-4">
-                            <img
-                                src="https://avatars.githubusercontent.com/u/54334789?v=4"
-                                alt="César"
-                                width="100"
-                                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
-                            />
-                            <h5 className="mb-0">Daniel David</h5>
-                            <span className="small text-uppercase text-muted">
-                                Programmer
-                            </span>
-                            <ul className="social mb-0 list-inline mt-3">
-                                <li className="list-inline-item">
-                                    <a
-                                        href="https://github.com/Danyboyyy"
-                                        className="social-link"
-                                    >
-                                        <FontAwesomeIcon icon={faGithub} />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    {/* <!-- Team item --> */}
-                    <div className="col-xl-3 col-sm-6 mb-5">
-                        <div className="bg-white rounded shadow-sm py-5 px-4">
-                            <img
-                                src="https://avatars.githubusercontent.com/u/78878007?v=4"
-                                alt="César"
-                                width="100"
-                                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
-                            />
-                            <h5 className="mb-0">Santiago Cano</h5>
-                            <span className="small text-uppercase text-muted">
-                                Programmer
-                            </span>
-                            <ul className="social mb-0 list-inline mt-3">
-                                <li className="list-inline-item">
-                                    <a
-                                        href="https://github.com/canosantiago12"
-                                        className="social-link"
-                                    >
-                                        <FontAwesomeIcon icon={faGithub} />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                        );
+                    })}
                 </div>
             </div>
         </>
