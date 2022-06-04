c486e879debce48a261a5774a72e9fe0f6e171f0
CesarMtzV <mc.cesar.art@gmail.com>
2022-06-04 11:45:17 -0500

    fix: Deleted borders used for development

--- src/views/ProfileView.js
+++ src/views/ProfileView.js
@@ -14,9 +14,9 @@ export const ProfileView = () => {
 
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-lg-4 text-center p-5 border border-primary">
+                    <div className="col-12 col-lg-4 text-center p-5">
                         <img
-                            className="img-fluid rounded-circle border border-secondary"
+                            className="img-fluid rounded-circle"
                             style={{ width: "200px", height: "200px" }}
                             alt="Profile"
                             src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmir-s3-cdn-cf.behance.net%2Fproject_modules%2Fdisp%2Fe034cb46865457.5867907fb1cae.png&f=1&nofb=1"
@@ -30,7 +30,7 @@ export const ProfileView = () => {
                         </button>
                     </div>
 
-                    <div className="col-12 col-lg-8 border p-5 border-danger">
+                    <div className="col-12 col-lg-8 p-5">
                         <h1 className="mb-5">Profile information</h1>
 
                         <h3>
